cc6e7844a271145f3a06d09076fa48776e7c8477
Samuel Curry <samwcurry@gmail.com>
2021-03-29 10:26:17 -0500

    Update Gamble.sol

--- Gamble.sol
+++ Gamble.sol
@@ -1,4 +1,3 @@
-
 pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -7,11 +6,15 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract Gamble {
     
-    mapping (address => uint) public userBlockNumber; 
-    mapping (address => uint) public userChoice;
-    mapping (address => uint) public userBetAmount;
-
     using SafeERC20 for IERC20;
+    
+    struct Bet {
+        uint blockNumber;
+        uint amount;
+        uint choice;
+    }
+    
+    mapping (address => Bet) public bets;
     
     uint public constant PERCENT_FEE = 5;
     uint public constant MAX_BET_AMOUNT_PERCENT = 10;
@@ -45,32 +48,32 @@ contract Gamble {
     function gamble(uint256 _amount, uint _userChoice) external returns(bool) {
         uint maxBetAmount = (BANK_ROLL * MAX_BET_AMOUNT_PERCENT)/100;
         require(_amount > 0, "Cannot bet 0 ziots.");
-        require(userBlockNumber[msg.sender] == 0, "Bet already pending.");
+        require(bets[msg.sender].blockNumber == 0, "Bet already pending.");
         require(_amount <= maxBetAmount, "Bet too large. Bet max 10% of balance.");
         ziotAddress.safeTransferFrom(msg.sender, address(this), _amount);
-        userBlockNumber[msg.sender] = block.number;
-        userChoice[msg.sender] = _userChoice;
-        userBetAmount[msg.sender] = _amount;
+        bets[msg.sender].blockNumber = block.number;
+        bets[msg.sender].choice = _userChoice;
+        bets[msg.sender].amount = _amount;
         BANK_ROLL += _amount;
         return true;
     }
     
     function getWinner() external returns(string memory) {
-        require(userBlockNumber[msg.sender] != 0, "No bets pending.");
-        require(block.number > userBlockNumber[msg.sender] + 5, "Not enough blocks have passed.");
-        if((uint256(blockhash(userBlockNumber[msg.sender]  + 5)) % 2) == userChoice[msg.sender]){
-            uint amountSendBack = ((userBetAmount[msg.sender]*(100-PERCENT_FEE))/100)*2;
+        require(bets[msg.sender].blockNumber != 0, "No bets pending.");
+        require(block.number > bets[msg.sender].blockNumber + 5, "Not enough blocks have passed.");
+        if((uint256(blockhash(bets[msg.sender].blockNumber  + 5)) % 2) == bets[msg.sender].choice){
+            uint amountSendBack = ((bets[msg.sender].amount*(100-PERCENT_FEE))/100)*2;
             ziotAddress.safeTransfer(msg.sender, amountSendBack);
             BANK_ROLL -= amountSendBack;
-            userBlockNumber[msg.sender] = 0;
-            userChoice[msg.sender] = 0;
-            userBetAmount[msg.sender] = 0;
-            return "won, very sad";
+            bets[msg.sender].blockNumber = 0;
+            bets[msg.sender].choice = 0;
+            bets[msg.sender].amount = 0;
+            return "u won u lucky cunt";
         } else {
-            userBlockNumber[msg.sender] = 0;
-            userChoice[msg.sender] = 0;
-            userBetAmount[msg.sender] = 0;
-            return "lost, very sad";
+            bets[msg.sender].blockNumber = 0;
+            bets[msg.sender].choice = 0;
+            bets[msg.sender].amount = 0;
+            return "u lost u dumb loser";
         }
         
     }
